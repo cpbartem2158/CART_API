a1ab3fa02c137ced6f96296c241e8e2ffb169cc4
cpbartem <cpbartem@yandex.by>
2026-04-23 15:40:54 +0300

    fix: update some files

--- internal/repository/create_cart.go
+++ internal/repository/create_cart.go
@@ -6,12 +6,12 @@ import (
 	"github.com/cpbartem2158/CART_API/internal/entity"
 )
 
-func (r *Repository) CreateCart(ctx context.Context) (*entity.Cart, error) {
+func (r *Repository) AddCart(ctx context.Context) (*entity.Cart, error) {
 
 	query := `
-   INSERT INTO carts DEFAULT VALUES
-   RETURNING id, created_at, updated_at
-`
+			INSERT INTO carts default values 
+			RETURNING id, created_at, updated_at`
+
 	var cart entity.Cart
 
 	err := r.db.QueryRowContext(ctx, query).Scan(
