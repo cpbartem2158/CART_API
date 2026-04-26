86d0649240f8f6466dce7c990f7e099fc85bc82c
cpbartem <cpbartem@yandex.by>
2026-04-26 22:30:21 +0300

    feat: add left join in view cart

--- internal/entity/cart_item.go
+++ internal/entity/cart_item.go
@@ -3,8 +3,8 @@ package entity
 import "time"
 
 type CartItem struct {
-	ID        int       `json:"id" db:"id"`
-	CartID    int       `json:"cart_id" db:"cart_id"`
+	ID        int64     `json:"id" db:"id"`
+	CartID    int64     `json:"cart_id" db:"cart_id"`
 	Product   string    `json:"product" db:"product"`
 	Price     float64   `json:"price" db:"price"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
